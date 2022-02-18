317d666f723eaafc6caf46b24609959851a478da
Hukadan <me@hukadan.org>
2022-02-18 21:42:24 +0100

    Use relative path

--- src/bin/export_json.rs
+++ src/bin/export_json.rs
@@ -3,7 +3,7 @@ extern crate serde_json;
 use pobsdlib::DataBase;
 
 fn main() {
-    let db_game = DataBase::new("/home/hukadan/Projects/Rust/Learning/pobsdlib/games.db");
+    let db_game = DataBase::new("games.db");
     let json_games = serde_json::to_string_pretty(&db_game.games).unwrap();
     println!("{}", json_games);
 }
