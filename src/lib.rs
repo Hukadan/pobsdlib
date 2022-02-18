6a830d7a3394e87d4d6480e44bfdb3a346306710
Hukadan <me@hukadan.org>
2022-02-18 08:14:40 +0100

    Start commenting

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! # pobsdlib
+//! A library to in interact with the PlayOnBSD database
+//! The database can be found at `https://github.com/playonbsd/OpenBSD-Games-Database`
+//!
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
