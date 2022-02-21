30ff466926b9081b177867ba3c81f18e614f3d19
Hukadan <me@hukadan.org>
2022-02-21 22:07:47 +0100

    Add some integration tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -18,3 +18,30 @@ fn test_game_get_by_id() {
         None => panic!("Game not found"),
     }
 }
+#[test]
+fn test_game_get_by_name() {
+    let db_game = DataBase::new("tests/data/test-games.db");
+    match db_game.games.get_item_by_name("Akane the Kunoichi") {
+        Some(game) => {
+            assert_eq!(game.id, 6);
+            assert_eq!(game.engine, "XNA".to_string());
+        }
+        None => panic!("Game not found"),
+    }
+}
+#[test]
+fn test_game_get_by_tag() {
+    let db_game = DataBase::new("tests/data/test-games.db");
+    let games = db_game.get_games_by_tag("indie");
+    assert_eq!(games.len(),2);
+    assert_eq!(games[0].name, "The Adventures of Shuggy".to_string());
+    assert_eq!(games[1].name, "Aeternum".to_string());
+}
+#[test]
+fn test_game_get_by_genre() {
+    let db_game = DataBase::new("tests/data/test-games.db");
+    let games = db_game.get_games_by_genre("RPG");
+    assert_eq!(games.len(),2);
+    assert_eq!(games[0].name, "Aedemphia".to_string());
+    assert_eq!(games[1].name, "Always Sometimes Monsters".to_string());
+}
