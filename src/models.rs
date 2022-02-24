f5986fd1cc6a9ba563a27ca1c5eb9949eae33e8f
Hukadan <me@hukadan.org>
2022-02-24 23:01:01 +0100

    get_field does not need to be in a Trait

--- src/models.rs
+++ src/models.rs
@@ -16,7 +16,6 @@ pub trait GameTraitsMut {
 pub trait GameTraits {
     fn get_tags(&self) -> &Vec<String>;
     fn get_genres(&self) -> &Vec<String>;
-    fn get_field(&self, name: &str) -> Field;
     fn field_contains(&self, field_name: &str, field_value: &str) -> bool;
 }
 
@@ -211,6 +210,43 @@ impl Game {
     pub fn new() -> Self {
         Self::default()
     }
+    fn get_field(&self, name: &str) -> Field {
+        match name {
+            "Game" => Field::NewGame(&self.name),
+            "Cover" => Field::SingleItem(&"Cover", &self.cover),
+            "Engine" => Field::SingleItem(&"Engine", &self.engine),
+            "Setup" => Field::SingleItem(&"Setup", &self.setup),
+            "Runtime" => Field::SingleItem(&"Runtime", &self.runtime),
+            "Hints" => Field::SingleItem(&"Hints", &self.hints),
+            "Year" => Field::SingleItem(&"Year", &self.year),
+            "Dev" => Field::SingleItem(&"Dev", &self.dev),
+            "Pub" => Field::SingleItem(&"Pub", &self.publi),
+            "Version" => Field::SingleItem(&"Version", &self.version),
+            "Status" => Field::SingleItem(&"Status", &self.status),
+            "Store" => {
+                let mut stores: Vec<&str> = Vec::new();
+                for store in &self.store {
+                    stores.push(store);
+                }
+                Field::MultipleItems(&"Store", stores)
+            }
+            "Genre" => {
+                let mut genres: Vec<&str> = Vec::new();
+                for genre in &self.genres {
+                    genres.push(genre);
+                }
+                Field::MultipleItems(&"Genre", genres)
+            }
+            "Tags" => {
+                let mut tags: Vec<&str> = Vec::new();
+                for tag in &self.tags {
+                    tags.push(tag);
+                }
+                Field::MultipleItems(&"Tags", tags)
+            }
+            _ => panic!("Unkown filed {}", name),
+        }
+    }
 }
 impl ItemTraitsMut for Game {
     /// Sets the id of the game.
@@ -305,43 +341,6 @@ impl GameTraits for Game {
     fn get_genres(&self) -> &Vec<String> {
         &self.genres
     }
-    fn get_field(&self, name: &str) -> Field {
-        match name {
-            "Game" => Field::NewGame(&self.name),
-            "Cover" => Field::SingleItem(&"Cover", &self.cover),
-            "Engine" => Field::SingleItem(&"Engine", &self.engine),
-            "Setup" => Field::SingleItem(&"Setup", &self.setup),
-            "Runtime" => Field::SingleItem(&"Runtime", &self.runtime),
-            "Hints" => Field::SingleItem(&"Hints", &self.hints),
-            "Year" => Field::SingleItem(&"Year", &self.year),
-            "Dev" => Field::SingleItem(&"Dev", &self.dev),
-            "Pub" => Field::SingleItem(&"Pub", &self.publi),
-            "Version" => Field::SingleItem(&"Version", &self.version),
-            "Status" => Field::SingleItem(&"Status", &self.status),
-            "Store" => {
-                let mut stores: Vec<&str> = Vec::new();
-                for store in &self.store {
-                    stores.push(store);
-                }
-                Field::MultipleItems(&"Store", stores)
-            }
-            "Genre" => {
-                let mut genres: Vec<&str> = Vec::new();
-                for genre in &self.genres {
-                    genres.push(genre);
-                }
-                Field::MultipleItems(&"Genre", genres)
-            }
-            "Tags" => {
-                let mut tags: Vec<&str> = Vec::new();
-                for tag in &self.tags {
-                    tags.push(tag);
-                }
-                Field::MultipleItems(&"Tags", tags)
-            }
-            _ => panic!("Unkown filed {}", name),
-        }
-    }
     fn field_contains(&self, field_name: &str, field_value: &str) -> bool {
         match &self.get_field(field_name) {
             Field::NewGame(value) => value.to_lowercase().contains(&field_value.to_lowercase()),
@@ -364,43 +363,6 @@ impl GameTraits for &Game {
     /// Returns the tag vector of the game.
     fn get_genres(&self) -> &Vec<String> {
         &self.genres
-    }
-    fn get_field(&self, name: &str) -> Field {
-        match name {
-            "Game" => Field::NewGame(&self.name),
-            "Cover" => Field::SingleItem(&"Cover", &self.cover),
-            "Engine" => Field::SingleItem(&"Engine", &self.engine),
-            "Setup" => Field::SingleItem(&"Setup", &self.setup),
-            "Runtime" => Field::SingleItem(&"Runtime", &self.runtime),
-            "Hints" => Field::SingleItem(&"Hints", &self.hints),
-            "Year" => Field::SingleItem(&"Year", &self.year),
-            "Dev" => Field::SingleItem(&"Dev", &self.dev),
-            "Pub" => Field::SingleItem(&"Pub", &self.publi),
-            "Version" => Field::SingleItem(&"Version", &self.version),
-            "Status" => Field::SingleItem(&"Status", &self.status),
-            "Store" => {
-                let mut stores: Vec<&str> = Vec::new();
-                for store in &self.store {
-                    stores.push(store);
-                }
-                Field::MultipleItems(&"Store", stores)
-            }
-            "Genre" => {
-                let mut genres: Vec<&str> = Vec::new();
-                for genre in &self.genres {
-                    genres.push(genre);
-                }
-                Field::MultipleItems(&"Genre", genres)
-            }
-            "Tags" => {
-                let mut tags: Vec<&str> = Vec::new();
-                for tag in &self.tags {
-                    tags.push(tag);
-                }
-                Field::MultipleItems(&"Tags", tags)
-            }
-            _ => panic!("Unkown filed {}", name),
-        }
     }
     fn field_contains(&self, field_name: &str, field_value: &str) -> bool {
         match &self.get_field(field_name) {
