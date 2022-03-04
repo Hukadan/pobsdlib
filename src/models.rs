180237f0056969f3b18056479b77360da3636db3
Hukadan <me@hukadan.org>
2022-03-04 22:46:32 +0100

    Traits updated, get_field made case insensitive and doc added/updated

--- src/models.rs
+++ src/models.rs
@@ -18,6 +18,7 @@ use crate::utils::split_line;
 /// let field = Field::from(line_str);
 ///
 /// assert_eq!(field,Field::NewGame(&"Name of the game"));
+/// assert_eq!(field.as_line(),line_str);
 /// ```
 ///
 /// ## Field::SingleItem
@@ -30,6 +31,7 @@ use crate::utils::split_line;
 /// let field = Field::from(line_str);
 ///
 /// assert_eq!(field,Field::SingleItem(&"Engine",&"Engine name"));
+/// assert_eq!(field.as_line(),line_str);
 /// ```
 ///
 /// ## Field::MultipleItems
@@ -38,10 +40,11 @@ use crate::utils::split_line;
 /// ```
 /// use pobsdlib::models::Field;
 ///
-/// let line_str = "Tags\ttag1,tag2";
+/// let line_str = "Tags\ttag1, tag2";
 /// let field = Field::from(line_str);
 ///
 /// assert_eq!(field,Field::MultipleItems(&"Tags",vec![&"tag1",&"tag2"]));
+/// assert_eq!(field.as_line(),line_str);
 /// ```
 /// Note that while Tags and Genres are coma separated values, Stores are space separated ones.
 /// This is handled by the `Field::from` method.
@@ -53,7 +56,15 @@ pub enum Field<'a> {
 }
 
 impl<'a> Field<'a> {
-    /// Try to convert a line of the database in a Field enum. Panic if it cannot.
+    /// Try to convert a line of the database in a Field enum (see exemple above). Panic if it cannot.
+    /// ```
+    /// use pobsdlib::models::Field;
+    ///
+    /// let line_str = "Tags\ttag1, tag2";
+    /// let field = Field::from(line_str);
+    ///
+    /// assert_eq!(field,Field::MultipleItems(&"Tags",vec![&"tag1",&"tag2"]));
+    /// ```
     pub fn from(line: &'a str) -> Self {
         // split the line in a left and right hand sides
         let (left, right) = split_line(line);
@@ -84,10 +95,10 @@ impl<'a> Field<'a> {
     /// use pobsdlib::models::Field;
     /// let input = "Engine\tSuper engine";
     /// let field = Field::from(&input);
-    /// assert_eq!(field.as_line(), input.to_string());
+    /// assert_eq!(field.as_line(), input);
     /// let input = "Genre\tGe1, Ge2";
     /// let field = Field::from(&input);
-    /// assert_eq!(field.as_line(), input.to_string());
+    /// assert_eq!(field.as_line(), input);
     /// ```
     pub fn as_line(&'a self) -> String {
         match self {
@@ -105,17 +116,31 @@ impl<'a> Field<'a> {
 }
 
 /* ------------------------ ITEM -------------------------*/
-/// This trait is needed if you use ItemCollection with Item or Game struct.
+/// This trait is needed if you use ItemCollection with Item.
 pub trait ItemTraits {
     fn get_name(&self) -> &str;
 }
-
-pub trait ItemTraitsMut {
+/// This trait is needed if you use ItemCollection with mutable Item.
+/// It implies ItemTraits
+pub trait ItemTraitsMut: ItemTraits {
     fn set_id(&mut self, id: usize);
 }
 
 /// # Represent an item.
 /// At the moment, only tags and genres are represented this way.
+///
+/// You need to bring ItemTraits to use get_name method
+/// ```
+/// use pobsdlib::models::{Item, ItemTraits};
+/// let mut item = Item::new();
+/// item.id = 1;
+/// item.name = "Item name".to_string();
+/// item.games.push(1);
+/// item.games.push(2);
+/// item.games.push(3);
+///
+/// assert_eq!(item.get_name(),"Item name");
+/// ```
 #[derive(Default, PartialEq)]
 pub struct Item {
     /// The id of the tag.
@@ -147,6 +172,13 @@ impl ItemTraits for &Item {
     }
 }
 
+impl ItemTraits for &mut Item {
+    /// Returns the name of the item.
+    fn get_name(&self) -> &str {
+        &self.name
+    }
+}
+
 impl ItemTraitsMut for Item {
     /// Sets the id of the item.
     fn set_id(&mut self, id: usize) {
@@ -163,17 +195,64 @@ impl ItemTraitsMut for &mut Item {
 
 /* ------------------------ GAME -------------------------*/
 /// This trait is needed if you use ItemCollection with Game struct.
+/// Implies ItemTraits
 pub trait GameTraits: ItemTraits {
     fn get_tags(&self) -> &Vec<String>;
     fn get_genres(&self) -> &Vec<String>;
     fn field_contains(&self, field_name: &str, field_value: &str) -> bool;
 }
 
+/// This trait is needed if you use ItemCollection wit a mutable Game struct.
+/// Implies ItemTraitsMut
 pub trait GameTraitsMut: ItemTraitsMut {
     fn update(&mut self, field: Field);
 }
 
 /// # Represent a game
+/// A Game is create by a line starting by Game in the database.
+/// Once created, the fiels are set by the following lines.
+/// This process can be simulated by the following lines
+/// ```
+/// // we use the update method which needs the GameTraitsMut Trait
+/// use pobsdlib::models::{Field, Game, GameTraitsMut};
+/// let database="Game	AaaaaAAaaaAAAaaAAAAaAAAAA!!! for the Awesome
+/// Cover	AaaaaA_for_the_Awesome_Cover.jpg
+/// Engine
+/// Setup
+/// Runtime	HumblePlay
+/// Store	https://www.humblebundle.com/store/aaaaaaaaaaaaaaaaaaaaaaaaa-for-the-awesome
+/// Hints	Demo on HumbleBundle store page
+/// Genre
+/// Tags
+/// Year	2011
+/// Dev
+/// Pub
+/// Version
+/// Status";
+/// let mut game = Game::new();
+/// // the update method takes a Field enum and update the Game fields accordingly
+/// for line in database.lines() {
+///     game.update(Field::from(line));
+/// }
+/// assert_eq!(game.name,"AaaaaAAaaaAAAaaAAAAaAAAAA!!! for the Awesome");
+/// assert_eq!(game.cover,"AaaaaA_for_the_Awesome_Cover.jpg");
+/// assert_eq!(game.engine,"");
+/// assert_eq!(game.setup,"");
+/// assert_eq!(game.runtime,"HumblePlay");
+/// assert_eq!(game.store,vec!["https://www.humblebundle.com/store/aaaaaaaaaaaaaaaaaaaaaaaaa-for-the-awesome"]);
+/// assert_eq!(game.hints,"Demo on HumbleBundle store page");
+/// assert_eq!(game.genres,vec![""]);
+/// assert_eq!(game.tags,vec![""]);
+/// assert_eq!(game.year,"2011");
+/// assert_eq!(game.dev,"");
+/// assert_eq!(game.publi,"");
+/// assert_eq!(game.version,"");
+/// assert_eq!(game.status,"");
+/// // you also can use the get_field method to get the corresponding Field enum
+/// assert_eq!(game.get_field("Year"), Field::SingleItem("Year","2011"));
+/// // get_field is not case sensitive
+/// assert_eq!(game.get_field("yEaR"), Field::SingleItem("Year","2011"));
+/// ```
 #[derive(Serialize, Default, PartialEq)]
 pub struct Game {
     /// The id of the game.
@@ -213,34 +292,35 @@ impl Game {
     pub fn new() -> Self {
         Self::default()
     }
-    fn get_field(&self, name: &str) -> Field {
-        match name {
-            "Game" => Field::NewGame(&self.name),
-            "Cover" => Field::SingleItem("Cover", &self.cover),
-            "Engine" => Field::SingleItem("Engine", &self.engine),
-            "Setup" => Field::SingleItem("Setup", &self.setup),
-            "Runtime" => Field::SingleItem("Runtime", &self.runtime),
-            "Hints" => Field::SingleItem("Hints", &self.hints),
-            "Year" => Field::SingleItem("Year", &self.year),
-            "Dev" => Field::SingleItem("Dev", &self.dev),
-            "Pub" => Field::SingleItem("Pub", &self.publi),
-            "Version" => Field::SingleItem("Version", &self.version),
-            "Status" => Field::SingleItem("Status", &self.status),
-            "Store" => {
+    /// Given a field name, return the corresponding Field enum
+    /// It is not case sensitive
+    pub fn get_field(&self, name: &str) -> Field {
+        match name.to_lowercase().as_str() {
+            "cover" => Field::SingleItem("Cover", &self.cover),
+            "engine" => Field::SingleItem("Engine", &self.engine),
+            "setup" => Field::SingleItem("Setup", &self.setup),
+            "runtime" => Field::SingleItem("Runtime", &self.runtime),
+            "hints" => Field::SingleItem("Hints", &self.hints),
+            "year" => Field::SingleItem("Year", &self.year),
+            "dev" => Field::SingleItem("Dev", &self.dev),
+            "pub" => Field::SingleItem("Pub", &self.publi),
+            "version" => Field::SingleItem("Version", &self.version),
+            "status" => Field::SingleItem("Status", &self.status),
+            "store" => {
                 let mut stores: Vec<&str> = Vec::new();
                 for store in &self.store {
                     stores.push(store);
                 }
                 Field::MultipleItems("Store", stores)
             }
-            "Genre" => {
+            "genre" => {
                 let mut genres: Vec<&str> = Vec::new();
                 for genre in &self.genres {
                     genres.push(genre);
                 }
                 Field::MultipleItems("Genre", genres)
             }
-            "Tags" => {
+            "tags" => {
                 let mut tags: Vec<&str> = Vec::new();
                 for tag in &self.tags {
                     tags.push(tag);
@@ -260,6 +340,13 @@ impl ItemTraits for Game {
 }
 
 impl ItemTraits for &Game {
+    /// Returns the name of the game.
+    fn get_name(&self) -> &str {
+        &self.name
+    }
+}
+
+impl ItemTraits for &mut Game {
     /// Returns the name of the game.
     fn get_name(&self) -> &str {
         &self.name
@@ -289,7 +376,7 @@ impl GameTraitsMut for Game {
     /// let field = Field::from(line_str);
     /// let mut game = Game::new();
     /// game.update(field);
-    /// assert_eq!(game.name,"Name of the game".to_string());
+    /// assert_eq!(game.name,"Name of the game");
     /// ```
     /// The id cannot be set this way and the `set_id` method must be used.
     fn update(&mut self, field: Field) {
